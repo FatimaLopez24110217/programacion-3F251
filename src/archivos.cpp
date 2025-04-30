2690647561517beb60cc556a365175571d4ba5c3
FatimaLopez24110217 <a24110217@live.ceti.mx>
2025-04-30 00:14:17 +0000

    Practica 5

--- src/archivos.cpp
+++ src/archivos.cpp
@@ -3,6 +3,12 @@
 #include <string>
 using namespace std;
 
+struct Persona
+{
+    char nombre[10];
+    int edad;
+};
+
 int main (int argc, char const *argv[])
 {
     ofstream archivoSalida("archivo.txt");
@@ -36,5 +42,25 @@ int main (int argc, char const *argv[])
         cout << linea << endl;
     }
     archivoEntrada.close();
+
+    //......................................................................................
+    //Escritura de archivos BINARIOS
+    ofstream archivoSalidaBinario("archivo.bin", ios::binary);
+    if(!archivoSalidaBinario) 
+    {
+        cerr << "Error al leer archivo" << endl;
+        return 1; // Finalizar con error
+    }   
+
+    Persona p1 = {"Juan", 20};
+    Persona p2 = {"Pedro", 30};
+    Persona p3 = {"Maria", 40};
+
+    archivoSalidaBinario.write((char*)(&p1), sizeof(Persona));
+    archivoSalidaBinario.write((char*)(&p2), sizeof(Persona));
+    archivoSalidaBinario.write((char*)(&p3), sizeof(Persona));
+    
+    archivoSalidaBinario.close();
+
     return 0;
 }
